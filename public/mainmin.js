48a6d9997cca2449888161f7e309bad5df024608
Chan11 <pchandrahas10@gmail.com>
2016-11-30 17:39:58 +0530

    ignore

--- public/mainmin.js
+++ public/mainmin.js
@@ -1,3 +1,4 @@
 var pageTitle=document.getElementById("pageTitle")
+var dom=document.getElementById('ol')
 axios.get("https://polling-app-chan.herokuapp.com/allquestions").then(function(e){var t=e.data.map(function(e){dom.insertAdjacentHTML("afterbegin",'<li><a href="/'+e.url+'">'+e.data[0]+"</a></li>")})
 pageTitle.insertAdjacentHTML("afterend","<h3>Total Number of Questions: "+t.length+"</h3>")})

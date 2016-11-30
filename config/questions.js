ff1c0aa2777b78fa59fbfda80e0c16dbecbb8706
Chan11 <pchandrahas10@gmail.com>
2016-11-30 17:35:54 +0530

    ignore

--- config/questions.js
+++ config/questions.js
@@ -1,4 +1,4 @@
-var db=require('./db.js');
+var db=require('./dbmin.js');
 var easyMongo = db.easyMongo;
 var questions=Object.create(null);
 var loaded = false;
--- public/mainmin.js
+++ public/mainmin.js
@@ -1,4 +1,3 @@
-"use strict"
-var dom=document.getElementById("ol"),pageTitle=document.getElementById("pageTitle")
-axios.get("https://polling-app-chan.herokuapp.com/allquestions").then(function(e){var a=e.data.map(function(e){console.log(e.data[0]),dom.insertAdjacentHTML("afterbegin",'<li><a href="/'+e.url+'">'+e.data[0]+"</a></li>")})
-pageTitle.insertAdjacentHTML("afterend","<h3>Total Number of Questions: "+a.length+"</h3>")})
+var pageTitle=document.getElementById("pageTitle")
+axios.get("https://polling-app-chan.herokuapp.com/allquestions").then(function(e){var t=e.data.map(function(e){dom.insertAdjacentHTML("afterbegin",'<li><a href="/'+e.url+'">'+e.data[0]+"</a></li>")})
+pageTitle.insertAdjacentHTML("afterend","<h3>Total Number of Questions: "+t.length+"</h3>")})
